6781260a92b85a6bb238b1324211770fea339ba6
berger <robertberger.dev@gmail.com>
2023-02-28 11:10:40 -0500

    added routing for Home.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AboutUs from './AboutUs'
+import Home from './Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="about-us" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
